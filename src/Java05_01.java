c685dd8770a7d52d2e619b18aa00cc92b92349f9
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-30 16:15:54 +0900

    5章 *memo* 1.for文、while文、if文、switch文、break文、continue文の構成はC言語と同じ 2.Javaにはラベル付きbreak文なるものがある。（Goto的な）

---
+++ src/Java05_01.java
@@ -0,0 +1,12 @@
+
+public class Java05_01 {
+	public static void main(String args[]) {
+		
+		int i;
+		
+		for(i = 0; i < 10; i++) {
+			System.out.println("処理は"+i+"回目です");
+		}
+		
+	}
+}
---
+++ src/Java05_02.java
@@ -0,0 +1,13 @@
+
+public class Java05_02 {
+	public static void main(String args[]) {
+	
+		int i = 10;
+		
+		while(i > 0) {
+			System.out.println("処理は"+i+"回目です");
+			i--;
+		}
+	
+	}
+}
