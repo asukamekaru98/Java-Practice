b436909bd14a2fe3d167293dcd29b0463eea269c
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-02-06 11:05:33 +0900

    13-[5] throwというキーワードを用いると意図的に起こしたい例外を起こすことができる

---
+++ src/Java13_03.java
@@ -0,0 +1,14 @@
+
+public class Java13_03 {
+	public static void main (String args[]) {
+		try {
+			meth1();
+		}catch (NullPointerException e) {
+			 System.out.println("お゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛!!!!!!!!!!!!!! : "+e.getMessage());
+		}
+	}
+	
+	public static void meth1() {
+		throw new NullPointerException("ﾃｽﾃｨﾝｸﾞ");
+	}
+}
