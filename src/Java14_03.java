ab6b5d3dd2e924b635613c9083f5bbe8443b9f28
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-02-06 15:48:50 +0900

    14-[3] Threadは"クラス" Runnableは"インタフェース" １、implements でRunnableインタフェースを指定してクラスを作成 ２、クラス内でrun()メソッドを実装 ３、作成したクラスをインスタンス化 ４、インスタンス化したクラスをThreadのコンストラクタの引数として渡す ５、start()メソッドで実行
    14-[4]
    ThreadとRunnableの使い分け方としては、runメソッドだけ実装するならば「Runnable」
    runメソッド以外をオーバーライドするならば[Thread」
    という感じ。
    
    14-[5]
    マルチスレッドは、synchronizedで排他処理をすることができる。
    synchronizedはメソッドか文に付けられる。メソッドにしたばあい、メソッド全体に影響が出る。
    文の場合は、自由に指定できる。
    staticの有無でも変わる。staticでロックがつけられる。staticなしのときは同じインスタンスに対してロックが設定され
    staticがあるとき、同じクラスに対してロックが付けられる。
    
    14-[6]
    書き方
    修飾子　synchronized　戻り値　メソッド名　（引数）{...}

---
+++ src/Java14_03.java
@@ -0,0 +1,64 @@
+class test{
+	private static int var1 = 20;
+	
+	public synchronized void decrease(String name) {
+		
+		try { 
+			    var1--;
+			    System.out.print("var1 ="+ var1);
+			    Thread.sleep(1000);
+				
+		} catch(InterruptedException e) {
+			    System.out.println(e);
+		}finally{
+			    System.out.println(" "+name+"の処理が終了");
+		}//end try
+		
+	}//end func
+	
+}//end class
+
+class test2 extends Thread{
+    test obj;
+    String name;
+	
+    test2(test obj, String name){
+		this.obj = obj;
+		this.name = name;
+	}
+	
+	public void run() {
+		for(int i = 0; i < 5; i++){
+		    obj.decrease(name);
+		}
+		
+	}//end func
+	
+	
+}//end class
+
+public class Java14_03 {
+	public static void main(String args[]) {
+	    test obj = new test();
+	    test2 tt1 = new test2(obj, "A");
+	    test2 tt2 = new test2(obj, "B");
+	    test2 tt3 = new test2(obj, "C");
+	    test2 tt4 = new test2(obj, "D");
+		
+	    System.out.println("開始");
+		
+	    tt1.start();
+	    tt2.start();
+	    tt3.start();
+	    tt4.start();
+		
+		try{
+		    tt1.join();
+		    tt2.join();
+		    tt3.join();
+		    tt4.join();
+		}catch(InterruptedException e){}
+	    System.out.println("終了");
+	}//end func
+	
+}//end class
